2817cad853e2a8b7a7ba0e9bc9d29e569e82fbd6
Limjunghwa <nin0405@naver.com>
2018-04-17 17:38:53 +0900

    first commit start youtube_react

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//create component
+const App = function(){
+  return<div>hi!?</div>;
+}
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
