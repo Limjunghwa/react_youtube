ccccde79861589cbf025ad40fc5ff85179d168dc
Limjunghwa <nin0405@naver.com>
2018-04-21 11:39:09 +0900

    video serach 함수 처리 index.js에 추가

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+const API_KEY="AIzaSyD-Idpvv6mPXEqz1dAo5XxYp2pu3mqEvV4";
+
 //create component
-const App = function(){
-  return<div>hi!?</div>;
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={videos:[]};//비디오라는 배열 초기화
+    YTSearch({key:API_KEY,term : 'surfboards'},(videos) => {
+      this.setState({videos}); //key 와 value가 같으면 하나로 줄여서 작성 가능
+      //this.setState({videos : videos});
+    });
+
+  }
+
+  render(){
+    return(
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App/>,document.querySelector('.container'));
+
+ReactDOM.render(<App />,document.querySelector('.container'));
